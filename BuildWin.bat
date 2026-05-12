5d6389ea55ac9b68340658b08eec19668777d86a
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-05-12 12:24:21 +0200

    Act 1

---
+++ BuildWin.bat
@@ -0,0 +1,3 @@
+Clear.bat
+python -m PyInstaller --onefile --windowed --noconsole --icon=yandere-simulator.ico NoCompatibleToInstall.py
+echo 1.0.0 > GameVersion.txt
---
+++ BuildWin.ps1
@@ -0,0 +1,3 @@
+./Clear.ps1
+python -m PyInstaller --onefile --windowed --noconsole --icon=yandere-simulator.ico NoCompatibleToInstall.py
+echo 1.0.0 > GameVersion.txt
